c40b82dff3666983edd5fbb8616e3b52c8184e92
Merdi Kim <merdikimuha1@gmail.com>
2025-01-03 02:25:57 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Arweave Africa Official Website
---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='h-screen w-full bg-black flex items-center justify-center'>
+      <h1 className='text-4xl font-bold text-orange-700'>Arweave Africa Official Website coming soon </h1>
+    </div>
+  )
+}
+
+export default App
