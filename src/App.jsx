ffe0e3f1c1522ce6a9f2bb01c24ddda7e9090b67
Cedric Karungu <ckarungu921@gmail.com>
2025-01-07 23:29:34 +0200

    feat: add Heroes & Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,29 @@
-import './App.css'
+import './App.css';
+
+import About from './components/About.jsx';
+import Community from './components/Community.jsx';
+import Cta from './components/Cta.jsx';
+import Footer from './components/Footer.jsx';
+import Hero from './components/Hero.jsx';
+import Header from './components/Header.jsx';
+import Partner from './components/Partner.jsx';
+import Partners from './components/Partner.jsx';
 
 function App() {
-
-  return (
-    <div className='h-screen w-full bg-black flex items-center justify-center'>
-      <h1 className='text-4xl font-bold text-orange-700'>Arweave Africa Official Website coming soon </h1>
-    </div>
-  )
+	return (
+		<>
+			<div className=' m-0 hero bg-[rgba(28,28,28,0.99)]'>
+				<Header />
+				<Hero />
+			</div>
+			<About />
+			<Partners />
+			<Cta />
+			<Footer />
+			<Community />
+			<Partner />
+		</>
+	);
 }
 
-export default App
+export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
+	content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+	theme: {
+		extend: {
+			colors: {
+				primary: '#D97700',
+				secondary: '#212121',
+				white: '#FFFFFF',
+				gray: '#B3B3B3',
+				background: '#1A1A1A',
+			},
+		},
+	},
+	plugins: [],
+};
